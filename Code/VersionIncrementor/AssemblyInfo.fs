142d2c4645eac3f2c1e798b7f6635c66a977c589
alexey.brovko <alexey.brovko@fastdev.se>
2020-05-02 21:03:04 +0500

    Init

---
+++ Code/VersionIncrementor/AssemblyInfo.fs
@@ -0,0 +1,39 @@
+﻿namespace DeveloperTools.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("DeveloperTools")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("DeveloperTools")>]
+[<assembly: AssemblyCopyright("Copyright ©  $year$")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("97DA58C2-5506-410A-9B80-63F3C2D9EB40")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// 
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Code/VersionIncrementor/Program.fs
@@ -0,0 +1,38 @@
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+#if !DEBUG
+let baseFolder : string = Directory.GetCurrentDirectory()
+let fileName = "Properties\AssemblyInfo.cs"
+#else
+let baseFolder :string = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName
+let fileName = "TestFile.txt"
+#endif
+    
+let getFullPath = Path.Combine(baseFolder, fileName)
+
+let regexPattern = "(?<=(AssemblyVersion(\(\"))(((\d)\.){2}))" + "(.*)" + "(?=(.(\d)\"\)]))"
+let fullVersionRegexPattern = "(?<=(AssemblyVersion(\(\")))" + "(.*)" + "(?=(\"\)]))"
+
+let getFile =
+    File.ReadAllText(getFullPath)
+    
+let replaceBuildVersion (fileContent : string) : string =
+    let value = Int32.Parse(Regex.Match(fileContent, regexPattern).Value)
+    Regex.Replace(fileContent, regexPattern, (value + 1).ToString())
+
+let saveFile (fileContent : string) =
+    File.WriteAllText(getFullPath, fileContent)
+    
+let printBuildVersion (fileContent : string) =
+    Console.WriteLine "Build version has been updated"
+    Console.WriteLine("New version: " + Regex.Match(fileContent, fullVersionRegexPattern).Value)
+    
+
+[<EntryPoint>]
+let main argv =
+    let newContent = replaceBuildVersion (getFile)
+    saveFile (newContent)
+    printBuildVersion (getFile)
+    0
